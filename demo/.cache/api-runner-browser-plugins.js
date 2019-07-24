a797e83e52dc45b3306beb75f53b171611539123
Tim Hannes <thannes@netatwork.com>
2019-07-24 12:15:15 -0400

    fix gitignore

--- demo/.cache/api-runner-browser-plugins.js
+++
@@ -1,4 +0,0 @@
-module.exports = [{
-      plugin: require('/Users/thannes/sites/code/gatsby-theme-location-mapper/node_modules/gatsby-plugin-mdx/gatsby-browser.js'),
-      options: {"plugins":[]},
-    }]
--- demo/.cache/async-requires.js
+++
@@ -1,9 +0,0 @@
-// prefer default export if available
-const preferDefault = m => m && m.default || m
-
-exports.components = {
-  "component---theme-src-templates-all-locations-js": () => import("/Users/thannes/sites/code/gatsby-theme-location-mapper/theme/src/templates/all-locations.js" /* webpackChunkName: "component---theme-src-templates-all-locations-js" */),
-  "component---theme-src-templates-location-js": () => import("/Users/thannes/sites/code/gatsby-theme-location-mapper/theme/src/templates/location.js" /* webpackChunkName: "component---theme-src-templates-location-js" */),
-  "component---cache-dev-404-page-js": () => import("/Users/thannes/sites/code/gatsby-theme-location-mapper/demo/.cache/dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */)
-}
-
