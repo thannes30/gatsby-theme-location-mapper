935adf948cd27960088fbc24672fdb9f38b9bc64
Tim Hannes <thannes@netatwork.com>
2019-07-30 18:04:14 -0400

    fix script

--- deploy-github.js
+++ deploy-github.js
@@ -4,7 +4,7 @@ ghpages.publish(
   "public",
   {
     branch: "master",
-    repo: "https://github.com/thannes30/gatsby-theme-location-mapper",
+    repo: "https://github.com/thannes30/gatsby-theme-location-mapper.github.io.git",
   },
   () => {
     console.log("Deploy Complete!")
